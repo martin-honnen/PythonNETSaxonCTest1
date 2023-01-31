0813febefec20ac3630f5915482219e1ffad6bf4
Martin Honnen <martin.honnen@gmx.de>
2023-01-31 14:31:53 +0100

    Construct various documents and don't build single tree to show that iterator from Python API over XdmValue can be used with C# foreach loop.

--- Program.cs
+++ Program.cs
@@ -25,5 +25,25 @@ using (dynamic saxonproc = saxonche.PySaxonProcessor())
 
     Console.WriteLine(result);
 
+    xslt30Transformer = xslt30Processor.compile_stylesheet(stylesheet_text: @"<xsl:stylesheet xmlns:xsl='http://www.w3.org/1999/XSL/Transform' version='3.0' expand-text='yes'>
+<xsl:output indent='yes' build-tree='no'/>
+<xsl:template name='xsl:initial-template'>
+  <xsl:for-each select='1 to 5'>
+    <xsl:document>
+      <root>This is test {.} with {system-property('xsl:product-name')} {system-property('xsl:product-version')} at {current-dateTime()}</root>
+    </xsl:document>
+  </xsl:for-each>
+</xsl:template>
+</xsl:stylesheet>");
+
+    xslt30Transformer.set_result_as_raw_value(true);
+
+    dynamic xdmValue = xslt30Transformer.call_template_returning_value(template_name: null);
+
+    foreach (dynamic xdmItem in xdmValue)
+    {
+        Console.WriteLine(xdmItem);
+    }
+
 }
 
